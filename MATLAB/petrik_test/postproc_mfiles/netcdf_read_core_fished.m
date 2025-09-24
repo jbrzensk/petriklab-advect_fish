af62d2fcd62151b32a96faaf4476c3e62e0ef2fe
Colleen <cpetrik@users.noreply.github.com>
2025-09-24 12:08:42 -0700

    fix CC

--- MATLAB/petrik_test/postproc_mfiles/netcdf_read_core_fished.m
+++ MATLAB/petrik_test/postproc_mfiles/netcdf_read_core_fished.m
@@ -4,7 +4,7 @@ clear
 close all
 
 %%
-cfile = 'Dc_enc70-b200_m4-b175-k086_c20-b250_D075_J100_A050_Sm025_nmort1_BE08_noCC_RE00100';
+cfile = 'Dc_enc70-b200_m4-b175-k086_c20-b250_D075_J100_A050_Sm025_nmort1_BE08_CC80_RE00100';
 %fpath=['/Volumes/petrik-lab/Feisty/NC/Matlab_new_size/' cfile '/CORE/'];
 fpath=['/project/Feisty/NC/Matlab_new_size/' cfile '/CORE/'];
 
